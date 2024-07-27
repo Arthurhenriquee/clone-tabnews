699640d1543d33977a9882a68c7d1cdf7df0032f
Arthurhenriquee <105128236+Arthurhenriquee@users.noreply.github.com>
2024-07-27 03:44:06 +0000

    atualiza a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eloah, eu te amo. Se você me ama, me mande uma foto dando uma risadinha!!</h1>
+    return <h1>Parabéns Arthur, você fez um belo trabalho, muito bom garoto!</h1>
 }
 
 export default Home; 
