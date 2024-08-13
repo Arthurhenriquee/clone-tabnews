d9c5f8c66b1e329dcfedb7135071c7f44f02f29a
Arthurhenriquee <105128236+Arthurhenriquee@users.noreply.github.com>
2024-08-13 20:26:43 +0000

    intalar `jest and configurar script

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Parabéns Arthur, você fez um belo trabalho, muito bom garoto!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
 export default Home;
