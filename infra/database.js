ad5870f7b0bcb3ea58e575e1bb765a4d69917c3b
Arthurhenriquee <105128236+Arthurhenriquee@users.noreply.github.com>
2025-02-16 03:22:13 +0000

    fix: updated variables

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,17 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 async function query(queryObject) {
- let client;
-try {
-  client = await getNewClient()
-  const result = await client.query(queryObject);
-  return result;
-} catch (error) {
- console.error(error);
- throw error;
-} finally {
-  await client.end();
-}
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 async function getNewClient() {
@@ -21,7 +21,6 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
     ssl: getSSLValues(),
   });
 
@@ -31,11 +30,11 @@ async function getNewClient() {
 
 export default {
   query,
-  getNewClient
+  getNewClient,
 };
 
 function getSSLValues() {
-  if (process.env.POSTREGS_CA) {
+  if (process.env.POSTGRES_CA) {
     return {
       ca: process.env.POSTGRES_CA,
     };
